ccf0c71f96f8e3c2cde05ccb4cb7ba8cdcde07e0
takerbiker <shahranibinrahiman@gmail.com>
2019-10-17 23:19:45 +0800

    Edit - Mobile responsiveness for login page

--- Bears_dashboard/index.js
+++ Bears_dashboard/index.js
@@ -11,52 +11,76 @@ const config = {
 (async function() {
   let xero = new XeroClient(config);
 
+  // To create
   // await xero.contacts.create({
   //   Name: "Shahrani"
   // })
 
+  //To print out data results
   const result = await xero.contacts.get();
-  console.log(result.Contacts.Name)
+  let emailAddress = result.Contacts[2].EmailAddress;
+  console.log(emailAddress);
+
+
+  xeroClient.core.invoices.getInvoices()
+    .then(invoices => {
+      console.log("Invoics: " + invoices.length);
+    }).catch(err => {
+      console.log(err);
+    });
+
+
+    // for (let i=0;i<4;i++) {
+  //   console.log(result.Contact[i].EmailAddress)
+  //   // console.log(EmailAddress);
+  // }
+  // let invoices = result.Invoices.Date
+  // console.log(invoices);
+
+
+  // const result2 = await xero.invoices.get();
+  // console.log(result2.invoices[1].Contact.Name)
+
 })();
 
-app.get('/invoices', async function(req, res) {
-    authorizedOperation(req, res, '/invoices', function(xeroClient) {
-        xeroClient.invoices.get()
-            .then(function(result) {
-                res.render('invoices', {
-                    invoices: result.Invoices,
-                    active: {
-                        invoices: true,
-                        nav: {
-                            accounting: true
-                        }
-                    }
-                });
-            })
-            .catch(function(err) {
-                handleErr(err, req, res, 'invoices');
-            })
-
-    })
-});
-
-app.get('/repeatinginvoices', async function(req, res) {
-    authorizedOperation(req, res, '/repeatinginvoices', function(xeroClient) {
-        xeroClient.repeatingInvoices.get()
-            .then(function(result) {
-                res.render('repeatinginvoices', {
-                    repeatinginvoices: result.RepeatingInvoices,
-                    active: {
-                        repeatinginvoices: true,
-                        nav: {
-                            accounting: true
-                        }
-                    }
-                });
-            })
-            .catch(function(err) {
-                handleErr(err, req, res, 'repeatinginvoices');
-            })
-
-    })
-});
+// app.get('/invoices', async function(req, res) {
+//     authorizedOperation(req, res, '/invoices', function(xeroClient) {
+//         xeroClient.invoices.get()
+//             .then(function(result) {
+//                 res.render('invoices', {
+//                     invoices: result.Invoices,
+//                     active: {
+//                         invoices: true,
+//                         nav: {
+//                             accounting: true
+//                         }
+//                     }
+//                 });
+//             })
+//             .catch(function(err) {
+//                 handleErr(err, req, res, 'invoices');
+//             })
+//
+//     })
+// });
+//
+// app.get('/repeatinginvoices', async function(req, res) {
+//     authorizedOperation(req, res, '/repeatinginvoices', function(xeroClient) {
+//         xeroClient.repeatingInvoices.get()
+//             .then(function(result) {
+//                 res.render('repeatinginvoices', {
+//                     repeatinginvoices: result.RepeatingInvoices,
+//                     active: {
+//                         repeatinginvoices: true,
+//                         nav: {
+//                             accounting: true
+//                         }
+//                     }
+//                 });
+//             })
+//             .catch(function(err) {
+//                 handleErr(err, req, res, 'repeatinginvoices');
+//             })
+//
+//     })
+// });
